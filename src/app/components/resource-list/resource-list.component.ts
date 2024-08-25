2b4724dc553b89eaaaad2a03ed7379c50aa80442
Alice Khovrus <alice.khovrus@gmail.com>
2024-08-25 19:39:02 +0700

    start

---
+++ src/app/components/resource-list/resource-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './resource-list.component.html',
+  styleUrl: './resource-list.component.css'
+})
+export class ResourceListComponent {
+
+}
